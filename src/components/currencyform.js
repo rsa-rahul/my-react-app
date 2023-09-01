dc116007004f3af7e195606391e71b51a2ae4a99
rsa-rahul <srivastavar105.rs@gmail.com>
2023-09-02 02:56:16 +0530

    word randomized and redirecting to appropriate page

--- src/components/currencyform.js
+++ src/components/currencyform.js
@@ -1,10 +1,25 @@
 import React, { useState } from 'react';
-import { Navigate } from 'react-router-dom';
+import { Link, Navigate, useHistory } from 'react-router-dom';
+import FailedPage from './FailedPage';
 
+function customShuffleArray(array) {
+    const newArray = [...array];
+    for (let i = newArray.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [newArray[i], newArray[j]] = [newArray[j], newArray[i]]; // Swap elements
+    }
+    return newArray;
+}
 function CurrencyForm() {
     const validCurrencies = ['Euro', 'Dollar', 'Inr', 'Peso', 'Dirham'];
     const inValidCurrencies = ['EUR', 'IND', 'USA', 'afghanistan'];
-    const allWords = [...validCurrencies, ...inValidCurrencies];
+
+    const validCurrenciesWithIdentifier = validCurrencies.map(word => [word, "valid"]);
+    const invalidCurrenciesWithIdentifier = inValidCurrencies.map(word => [word, "invalid"]);
+    const allWords = [
+        ...validCurrenciesWithIdentifier, ...invalidCurrenciesWithIdentifier];
+
+    const shuffledWords = customShuffleArray(allWords);
 
     const handleCorrectWordClick = (word) => {
         Navigate("/result");
@@ -12,23 +27,20 @@ function CurrencyForm() {
     const handleInCorrectWordClick = (word) => {
         Navigate("/failedpage");
     }
+    const wordToLinkRenderer = (wordArray, index) => {
+        let word = wordArray[0];
+        let isValid = wordArray[1];
+        if(isValid==="valid"){
+            return <Link className="custom-link" to="/result" >{word} </Link>
+        }
+        else{
+            return <Link className="custom-link" to="/failedpage" >{word} </Link>
+        }
+    }
     return (
         <div>
             <h2>Select a valid currency</h2>
-            <ul>
-                {validCurrencies.map((word, index) => (
-                    <li key={index}>
-                        <button onClick={() => handleCorrectWordClick(word)}>{word}</button>
-                    </li>
-                ))}
-            </ul>
-            <ul>
-                {inValidCurrencies.map((word, index) => (
-                    <li key={index}>
-                        <button onClick={() => handleInCorrectWordClick(word)}>{word}</button>
-                    </li>
-                ))}
-            </ul>
+            {shuffledWords.map(wordToLinkRenderer)}
         </div>
     );
 }
